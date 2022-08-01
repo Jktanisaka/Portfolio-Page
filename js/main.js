d37c2efdf2995240deabbf12edca0f13f2005145
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-08-01 00:09:02 +0000

    updated projects to add hit-draft

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const printit = document.querySelector('#printit');
 const checkmate = document.querySelector('#checkmate');
 const graySquare = document.querySelector('#gray-square');
+const hitDraft = document.querySelector('#hit-draft');
 
 printit.addEventListener('mouseover', () => printit.setAttribute('src', './images/printit.gif'));
 printit.addEventListener('mouseout', () => printit.setAttribute('src', './images/printit.png'));
@@ -10,3 +11,6 @@ checkmate.addEventListener('mouseout', () => checkmate.setAttribute('src', './im
 
 graySquare.addEventListener('mouseover', () => graySquare.setAttribute('src', './images/graysquare.gif'));
 graySquare.addEventListener('mouseout', () => graySquare.setAttribute('src', './images/graysquare.png'));
+
+hitDraft.addEventListener('mouseover', () => hitDraft.setAttribute('src', './images/HitDraft.gif'));
+hitDraft.addEventListener('mouseout', () => hitDraft.setAttribute('src', './images/hitdraftlogo.png'));
